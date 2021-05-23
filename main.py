7f63afa6af0050d3252cb03d311cef19ac0dc85e
pi <example@example.com>
2021-05-23 14:39:07 +0200

    first version of the webservice

---
+++ main.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI, WebSocket
+from fastapi.responses import HTMLResponse
+import json
+import synth
+app = FastAPI()
+
+
+j = """{
+    "text": "hello world",
+    "textToSpectrogramModel": "Tacotron2-CassieLeeMorris",
+    "vocoder": "hifi_gan",
+    "sampleRate": 22050,
+    "responseFormat": ""
+}"""
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        text = None
+        try:
+            jsonData = json.loads(data)
+            text = jsonData["text"]
+            wav = synth.synth(text)
+
+        except KeyError:
+            pass
+            #TODO:handle error
+
+        except json.decoder.JSONDecodeError:
+            pass
+            #TODO:handle error
+
+        await websocket.send_text(f"Message text was: {text}")
+        print(type(wav)) # list type
+        # await websocket.send_bytes(wav) # doesn't work yet
---
+++ synth.py
@@ -0,0 +1,38 @@
+from TTS.utils.manage import ModelManager
+from TTS.utils.synthesizer import Synthesizer
+
+TEXT = "A list of open speech corpora for Speech Technology research and development."
+# use_cuda = True
+
+DEBUG = True
+use_cuda = False
+
+def synth(text="hello world."):
+
+    TTS_CONFIG = "voiceModel/tts_models--en--ljspeech--tacotron2-DDC/config.json"
+    TTS_MODEL = "voiceModel/tts_models--en--ljspeech--tacotron2-DDC/model_file.pth.tar"
+
+    VOCODER_MODEL = "voiceModel/vocoder_model--en--ljspeech-hifigan_v2/model_file.pth.tar"
+    VOCODER_CONFIG = "voiceModel/vocoder_model--en--ljspeech-hifigan_v2/config.json"
+
+    model_path = TTS_MODEL
+    config_path = TTS_CONFIG
+    vocoder_path = VOCODER_MODEL
+    vocoder_config_path = VOCODER_CONFIG
+
+    synthesizer = Synthesizer(model_path, config_path,
+                              vocoder_path, vocoder_config_path, use_cuda)
+
+    print(" > Text: {}".format(text))
+
+    wav = synthesizer.tts(text)
+    
+    if DEBUG:
+        out_path = "out/1.wav"
+        synthesizer.save_wav(wav, out_path)
+
+    return wav
+
+
+if __name__ == '__main__':
+    synth()
